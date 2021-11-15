b912ac45f5e27d7080d3654f88ee7018f9520117
kdel01 <delaverek@gmail.com>
2021-11-15 16:58:16 +1100

    added github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather </h1>
+      <p>
+        <a
+          href="https://github.com/kdel01/final-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github link
+        </a>
+      </p>
     </div>
   );
 }
