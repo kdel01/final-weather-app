ec2be481af0cc844162ab83f36dd17a266f03d4b
kdel01 <delaverek@gmail.com>
2021-11-15 17:33:43 +1100

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather </h1>
-      <p>
-        <a
-          href="https://github.com/kdel01/final-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github link
-        </a>
-      </p>
+      <div className="container">
+        <h1> Weather </h1>
+        <button className="btn btn-primary">Test</button>
+        <footer>
+          <a
+            href="https://github.com/kdel01/final-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github link
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
